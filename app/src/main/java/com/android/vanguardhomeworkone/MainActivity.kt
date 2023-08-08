27d06993e57b938bf521dcbfc96cc49423c85fde
cbh1992 <whgg9786@gmail.com>
2023-08-08 20:37:56 +0900

    updated : 2023-08-08

---
+++ app/src/main/java/com/android/vanguardhomeworkone/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.vanguardhomeworkone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//UI
+//Toolbar, TabLayout, ViewPager2, RecyclerView, FloatingActionButton
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
